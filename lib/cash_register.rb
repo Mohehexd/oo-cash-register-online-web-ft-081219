f540d70c7701e92930db1afe98b02b26cabea619
Guillermo Martinez <dumblemoe@gmail.com>
2019-09-01 22:56:12 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 class CashRegister
 
-  attr_accessor :total, :discount, :apply_discount, :cart, :last 
+  attr_accessor :total, :discount, :apply_discount, :cart, :last_transaction
 
   def initialize(discount = 0)
     @total = 0 
