8cf12fa4e2389bb2c61d325097a1d95f91a5a979
Guillermo Martinez <dumblemoe@gmail.com>
2019-09-01 21:55:12 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,7 @@
 class CashRegister
 
-def initialize(discount = 0)
-  @total = 0 
-  @discount = discount
-end 
+  def initialize(discount = 0)
+    @total = 0 
+    @discount = discount
+  end 
 end 
